430798474375d63f4b70e2acd83f943030e5ce36
Martinaeht <tiinii01@gmail.com>
2024-04-30 11:11:52 +0200

    submission of 2nd assessment

--- main.py
+++ main.py
@@ -3,11 +3,37 @@ Simple Calculator
 """
 # Provide your solution here
 
+def calculate(integer1:int, operator:str, integer2:int):
+    if operator == "+":
+        return(integer1 + integer2)
+    elif operator == "-":
+        return (integer1 - integer2)
+    elif operator == "*":
+        return (integer1*integer2)
+    elif operator == "/":
+        if integer2 != 0:
+            return (integer1/integer2)
+        elif integer2 == 0:
+            print("Division by 0 is not allowed")
+    else:
+        print("Invalid operator.")
+
+result = calculate(21, "+", 3)
+print(result)
+
+
 
 """
 Reverse Word
 """
 # Provide your solution here
+
+i = 0
+def reverse_word(word:str):
+    for i in range(len(word)):
+        print(word.capitalize()[len(word) - 1 - i])
+
+reverse_word("green")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
